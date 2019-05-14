cacee0c39c8ea069881f980d50e11a8e0f7c99d9
Oscar Chinellato <sickdevelopers@gmail>
2019-05-14 11:04:43 +0200

    add ecosystem

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -6,7 +6,23 @@ try {
 }
 
 module.exports = {
-    apps : [],
+    apps : [{
+        name: 'web',
+        script: 'src/index.js',
+
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        // args: 'one two',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '500M',
+        env: {
+            NODE_ENV: 'development',
+        },
+        env_production: {
+            NODE_ENV: 'production',
+        }
+    }],
 
     deploy
 };
