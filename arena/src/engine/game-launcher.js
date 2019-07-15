8c4b4b310f6ebb5654fa2ca3c1f69d825032b77e
Oscar Chinellato <oscar.chinellato@sickdevelopers.com>
2019-07-15 11:19:00 +0200

    Fix worker engine ref

--- arena/src/engine/game-launcher.js
+++ arena/src/engine/game-launcher.js
@@ -4,7 +4,7 @@ const launch = function(p1, p2) {
 
   if (isMainThread) {
 
-    const worker = new Worker('./engine/game-worker.js');
+    const worker = new Worker('./src/engine/game-worker.js');
     worker.postMessage(JSON.stringify([p1, p2]));
 
     return new Promise((resolve, reject) => {
